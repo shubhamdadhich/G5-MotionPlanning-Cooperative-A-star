0b75ce1cc39e64b5da897f21bb0df462c2dc907d
Tyler Silva <tyler.silva.ut@gmail.com>
2023-04-05 16:12:59 -0600

    2 robot run with no collisions

--- run_CA_star.py
+++ run_CA_star.py
@@ -22,9 +22,17 @@ if __name__ == '__main__':
     # create reservation table
     res_table = reservation_table()
 
-    # get path for robot 1 from CA*
-    r1_path, res_table_1 = run_CA_star('./map0.txt', res_table)
-    robot_id = 1
+    # get path for robot 1 from CA* and update reservation table with path
+    r1_path, res_table_1 = run_CA_star('./map_col_r1.txt', res_table)
 
-    print(res_table_1)
+    print(len(res_table_1))
 
+    # run CA* with next robot 
+    # get path for robot 2 from CA* and update reservation table with path
+    r2_path, res_table_2 = run_CA_star('./map_col_r2.txt', res_table)
+
+    print(len(res_table_2))
+    print(res_table_2)
+
+    # now we need to make sure collision avoidance is working and display it better
+    # also make sure heuristic is good
