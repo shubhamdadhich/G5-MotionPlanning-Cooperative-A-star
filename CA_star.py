f8167ea6e96b2bc2d0210c57c9dde5ce7498ece8
Tyler Silva <tyler.silva.ut@gmail.com>
2023-04-06 16:15:44 -0600

    CA*

--- CA_star.py
+++ CA_star.py
@@ -305,7 +305,7 @@ def CA_star_search(init_state, f, is_goal, actions, h, res_table, robot_id):
                 path, action_path = backpath(n_i)
                 # make robot 1 path 'reserved' in space-time 
                 for state in path:
-                    res_table.add(tuple(state), robot_id)
+                    res_table.add(tuple(state), robot_id)                    
                 return (path, visited, res_table)
             else:
                 for a in actions:
--- run_CA_star.py
+++ run_CA_star.py
@@ -26,16 +26,12 @@ if __name__ == '__main__':
     # get path for robot 1 from CA* and update reservation table with path
     r1_path, res_table_1 = run_CA_star('./map_col_r1.txt', res_table, robot_id = 1)
 
-    print(res_table.keys())
-
-    print(len(res_table_1))
     print(res_table_1)
 
     # run CA* with next robot 
     # get path for robot 2 from CA* and update reservation table with path
     r2_path, res_table_2 = run_CA_star('./map_col_r2.txt', res_table, robot_id = 2)
 
-    print(len(res_table_2))
     print(res_table_2)
 
     # NOTE reservation table in CA* script is in progress
