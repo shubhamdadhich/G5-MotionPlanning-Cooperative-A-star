d44d5493ad3ad75c831c03de3663aa4d8e602f03
shubham <dadhich.shubham@gmail.com>
2023-04-13 15:14:49 -0600

    Getting ca star from main

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
 # Central server class will be implemented here
+
+from map_reader import MapReader
+from 
+
+
+class RobotServer(object):
+
+    def __init__(self, mapFile) -> None:
+        self.mapFile = mapFile
+        self.mapReader = MapReader(mapFile=self.mapFile)
+
+
+    def startPlanning(self):
+
+        for id in range(self.mapReader.robotCount):
