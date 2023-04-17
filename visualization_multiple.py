58d31c0f327ade7abb90b8eb7a910ef1310fb406
shubham <dadhich.shubham@gmail.com>
2023-04-16 18:29:17 -0600

    correcting coords

--- visualization_multiple.py
+++ visualization_multiple.py
@@ -65,7 +65,7 @@ def animate_paths(paths, mapDim, goalset, obstacleList, file_name, show_goals = 
             # Plot goals
 
             for ind, setPt in enumerate(goalset):
-                ax.plot(setPt[1][0], setPt[1][1], color=colorList[ind], marker='^')
+                ax.plot(setPt[1][1], setPt[1][0], color=colorList[ind], marker='^')
 
             # ax.plot(2,4,color = 'black', marker = 'o')
             # ax.plot(2,0,color = 'gray', marker = 'o')
@@ -73,12 +73,12 @@ def animate_paths(paths, mapDim, goalset, obstacleList, file_name, show_goals = 
         # Plot Obstacles
 
         for point in obstacleList:
-            ax.plot(point[0], point[1], color='black', label='original', marker='s',markersize=20)
+            ax.plot(point[1], point[0], color='black', label='original', marker='s',markersize=20)
 
         # Plot that point using the x and y coordinates
 
         for ind, point in enumerate(apointList):
-            ax.plot(point[0], point[1], color=colorList[ind], label='original', marker='o',markersize=20)
+            ax.plot(point[1], point[0], color=colorList[ind], label='original', marker='o',markersize=20)
 
         # ax.plot(point1[0], point1[1], color='black', label='original', marker='o',markersize=40)
         # ax.plot(point2[0], point2[1], color='gray', label='original', marker='o',markersize=40)
