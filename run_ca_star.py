020c8af3689db8b14a955d36c3f29ce2d444d4ad
varunraveendra <56466294+varunraveendra@users.noreply.github.com>
2023-04-22 03:01:01 -0600

    Update run_ca_star.py

--- run_ca_star.py
+++ run_ca_star.py
@@ -5,19 +5,18 @@ from server import RobotServer
 
 map_root = "maps/"
 map_list = [
-            # "testmap1.txt", # 5x5, 4 robots  - works
-            # "bug_map.txt",  # 5x5, 4 robots  - works
-            # "vdberg1a.txt", # 5x5, 3 robots  - works
-            # "vdberg1b.txt", # 6x5, 3 robots  - doesn't work (no solution with our code)
-            # "vdberg1c.txt", # 6x5, 4 robots  - works
-            # "plus.txt",     # 7x7, 2 robots  - works
-            "choke.txt",    # 7x7, 2 robots  - works but shows error of the first robot blocking the path at its goal
-            # "testmap0.txt", # 10x10, 4 robots  - works
-            # "testmap2.txt", # 10x10, 4 robots  - works
-            # "vdberg3c.txt", # 32x22, 10 robots  - works
-            "quad.txt",     # 32x22, 4 robots  - works but still throws an error an error
-            ##"testmap1_bug.txt",  # 10x10, 4 robots  - grey gets stuck and it throws an error
-            "gauntlet.txt"  # 32x21, 19 robots  - doesn't work
+            #"testmap1.txt", # 5x5, 4 robots
+            #"bug_map.txt",  # 5x5, 4 robots
+            #"vdberg1a.txt", # 5x5, 3 robots
+            #"vdberg1b.txt", # 6x5, 3 robots
+            #"vdberg1c.txt", # 6x5, 4 robots
+            #"plus.txt",     # 7x7, 2 robots
+            #"choke.txt",    # 7x7, 2 robots
+            #"testmap0.txt", # 10x10, 4 robots
+            #"testmap2.txt", # 10x10, 4 robots
+            #"vdberg3c.txt" # 32x22, 10 robots
+            #"quad.txt",     # 32x22, 4 robots
+            "gauntlet.txt" # 32x21, 19 robots
 ]
 
 if __name__ == "__main__":
