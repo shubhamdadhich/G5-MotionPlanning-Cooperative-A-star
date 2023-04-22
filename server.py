584e86ac738a315c9e7f78829c3ef3be5ed3a710
varunraveendra <56466294+varunraveendra@users.noreply.github.com>
2023-04-22 02:58:27 -0600

    Update server.py

--- server.py
+++ server.py
@@ -20,10 +20,25 @@ class RobotServer(object):
         for id, goalSet in zip(range(self.mapReader.robotCount), self.mapReader.goalSet):
             robotObj = self.mapReader.robotType(id+_buffer)
             self.robotList = [id+_buffer, robotObj]
-            path, visited, self.resTable = robotObj.getPath(self.mapReader.mapDim, self.mapReader.initMap, self.resTable, goalSet[0], goalSet[1])
+            
+            #print(goalSet,"khkajBDFk")
+            #print(jdbn)
+            #tpath, tvisited, tresTable,mreqtime = robotObj.getPath(self.mapReader.mapDim, self.mapReader.initMap, self.resTable, goalSet[0], goalSet[1],200)
+            path, visited, self.resTable = robotObj.getPath(self.mapReader.mapDim, self.mapReader.initMap, self.resTable, goalSet[0], goalSet[1],100)
             self.paths.append(path)
-            print(path)
-        # print(self.paths)
+            print("Done")
+        
+        fh=open("output.txt",'w')
+        time=dict()
+        for pos in self.resTable:
+            if pos[2] in time:
+                time[pos[2]].append([self.resTable[pos],pos[0],pos[1]])
+            else:
+                time[pos[2]]=[]
+                time[pos[2]].append([self.resTable[pos],pos[0],pos[1]])
+
+        print(time,file=fh)
+        fh.close()
         return self.paths
     
     def animatePath(self, outPath):
